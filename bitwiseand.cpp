45c20ab44ffb6f84591205f9c7031706fe837375
sammita94 <sammitachakravarti@yahoo.com>
2015-11-28 19:34:27 +0530

    modified bitwise and of all pairs of numbers in an array

--- bitwiseand.cpp
+++ bitwiseand.cpp
@@ -1,23 +1,32 @@
-// A Simple C++ program to compute sum of bitwise AND 
+// An efficient C++ program to compute sum of bitwise AND
 // of all pairs
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
 // Returns value of "arr[0] & arr[1] + arr[0] & arr[2] + 
 // ... arr[i] & arr[j] + ..... arr[n-2] & arr[n-1]"
 int pairAndSum(int arr[], int n)
 {
     int ans = 0;  // Initialize result
-
-    // Consider all pairs (arr[i], arr[j) such that
-    // i < j
-    for (int i = 0; i < n; i++)
-        for (int j = i+1; j < n; j++)
-           ans += arr[i] & arr[j];
-
+ 
+    // Traverse over all bits
+    for (int i = 0; i < 32; i++)
+    {
+        // Count number of elements with i'th bit set
+        int k = 0;  // Initialize the count
+        for (int j = 0; j < n; j++)
+            if ( (arr[j] & (1 << i)) )
+                k++;
+ 
+        // There are k set bits, means k(k-1)/2 pairs.
+        // Every pair adds 2^i to the answer. Therefore,
+        // we add "2^i * [k*(k-1)/2]" to the answer.
+        ans += (1<<i) * (k*(k-1)/2);
+    }
+ 
     return ans;
 }
-
+ 
 // Driver program to test above function
 int main()
 {
