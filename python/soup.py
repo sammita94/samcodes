1e57dc88d4493ed7c9ce3ef93efa1e4287c7d477
sammita94 <sammitachakravarti@yahoo.com>
2015-12-25 18:50:50 +0530

    beautiful soup implementation

---
+++ python/soup.py
@@ -0,0 +1,11 @@
+import urllib
+import bs4
+import os
+url = raw_input('Enter - ')
+html = urllib.urlopen(url).read()
+soup = bs4.BeautifulSoup(html)
+# Retrieve a list of the anchor tags
+# Each tag is like a dictionary of HTML attributes
+tags = soup('a')
+for tag in tags:
+    print tag.get('href', None)
