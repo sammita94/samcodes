8294c35a45142d2ddcf4921403680fa8a24c6f57
sammita94 <sammitachakravarti@yahoo.com>
2016-03-26 21:06:06 +0530

    [Code Added]Python script for playing a video in loops

---
+++ python/break.py
@@ -0,0 +1,10 @@
+import time
+import webbrowser
+
+total_breaks = 3
+break_count = 0
+
+while(break_count < total_breaks):
+    time.sleep(60)
+    webbrowser.open("https://www.youtube.com/watch?v=98WtmW-lfeE")
+    break_count = break_count + 1
