5b2a29085e486b4c3455adfc3953d6d802b16c00
sammita94 <sammitachakravarti@yahoo.com>
2016-03-29 23:29:18 +0530

    [Code Added]File transfer Protocol

--- python/break.py
+++ python/break.py
@@ -5,6 +5,6 @@ total_breaks = 3
 break_count = 0
 
 while(break_count < total_breaks):
-    time.sleep(60)
+    time.sleep(600)
     webbrowser.open("https://www.youtube.com/watch?v=98WtmW-lfeE")
     break_count = break_count + 1
